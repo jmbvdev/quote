bcdb86d1cf1d9fb0563bb132012f7261720e14a5
jmbvdev <jmvb.dev@gmail.com>
2021-12-20 17:55:50 -0400

    se finalizo con el proyecto

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 import Autor from './components/Autor';
 import Quote from './components/Quote';
 import quotes from "./quotes.json"
 
 function App() {
-  const quote= quotes[0]
+  const getNumber=max=> Math.floor(Math.random()*max)
+  const colors=["#845EC2","#4B4453", "#B0A8B9", "#C34A36", "#008B74", "#936C00", "#B740AD", "#C493FF", "#2F4858", "#C3F9FF"]
+  const [quote, setAutor]= useState(getNumber(quotes.length))
+  const [background, setBackground]= useState(getNumber(colors.length))
+  const mainQuotes= quotes[quote]
+  const getRadomquotes = ()=>{
+    setAutor(getNumber(quotes.length))
+    setBackground(colors[getNumber(colors.length)])
+    
+  }
   return (
     <>
-    <div className='quote'>
+    <div className='container' style={{background}} >
+
+    <div className='quote' style={{color:`${background}`}} >
     <i class="fas fa-quote-right"></i>
-    <Quote frase={quote.quote}/>
-    <Autor autor= {quote.author}/>
-    <button><i class="fas fa-angle-right"></i></button>
+    <Quote frase={mainQuotes.quote} background={background}/>
+    <Autor autor= {mainQuotes.author} background={background}/>
+    <button onClick={getRadomquotes} style={{background}}><i class="fas fa-angle-right"></i></button>
+    </div>
     </div>
     </>
   );
